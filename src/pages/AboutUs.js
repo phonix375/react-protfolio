2889cd81b8f4096f038b5db790286a3f988794ec
Alexy Kotliar <kotliar.alex@gmail.com>
2021-08-16 23:32:06 -0400

    base code and start of the about page

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import { directive } from '@babel/types';
+import React from 'react';
+
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+    return(
+        <div>
+          <AboutSection />  
+        </div>
+    )
+}
+
+export default AboutUs;
