2b16b735eb6140131a03340f9116bebd880edccd
Alexy Kotliar <kotliar.alex@gmail.com>
2021-08-20 17:10:55 -0400

    started working on the routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 //global style
   import GlobalStyle from './components/GlobalStyle';
 
+  //import pages
+  import ContactUs from './pages/ContactUs'
+  import OurWork from './pages/OurWork';
+
+  //router
+  import {Switch, Route} from 'react-router-dom'
+
   import Nav from './components/Nav'
 //import pages
 import AboutUs from './pages/AboutUs'
@@ -10,7 +17,17 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Route path="/">
+        <AboutUs />
+      </Route>
+      <Route path="/work">
+        <OurWork />
+      </Route>
+      <Route path="/contact">
+        <ContactUs />
+      </Route>
+      
+      
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurWork = () => {
+    return(
+        <div>
+            <h1>This is our work page</h1>
+        </div>
+    )
+}
+
+export default OurWork;
