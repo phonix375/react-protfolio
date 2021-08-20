9a7bc4f5d2283f5c734dcce19436c5a6a3b36664
Alexy Kotliar <kotliar.alex@gmail.com>
2021-08-20 16:36:45 -0400

    added the nav with the style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 //global style
   import GlobalStyle from './components/GlobalStyle';
+
+  import Nav from './components/Nav'
 //import pages
 import AboutUs from './pages/AboutUs'
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
